0749feff24ea1db7c3af425856437de1ff4937fa
bquizza <blaine555@gmail.com>
2019-06-20 11:51:57 -0600

    dropdown.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,10 +3,10 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
+    this.element = element;
     // Get the custom data attribute on the Link
     // this.data;
-    
+    this.data = element.data;
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
     
@@ -59,4 +59,8 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll(".tabs-link");
+
+links.forEach( (link) => {
+  return new TabLink(link);
+});
